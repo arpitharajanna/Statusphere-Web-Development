d2e7525d494479490420b7833cdb3684cc817d31
Gaurav Kataria <gaurav.kataria@panderasystems.com>
2017-03-25 21:17:24 -0400

    Register and login functionality with encryption and security

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,65 @@
 var express = require('express');
 var router = express.Router();
 var mongodb= require('mongodb');
-console.log("Welcome")
+var jwt = require('express-jwt');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+console.log("Welcome")
+var auth = jwt({
+  secret: 'MY_SECRET',
+  userProperty: 'payload'
 });
 
-router.get('/thelist',function(req,res){
-	var MongoClient= mongodb.MongoClient;
-	var url = 'mongodb://localhost:27017/yesy_1'
-	MongoClient.connect(url,function(err,db){
-		if(err){
-			console.log('Unable to connect to the server',err);
-		}
-		else{
-			console.log("Connection Established",url);
-			var collection=db.collection('students');
-			collection.find({}).toArray(function(err,result){
-				if(err){
-					res.send(err);
-				}
-				else if(result.length) {
+var ctrlProfile = require('../controllers/profile');
+var ctrlAuth = require('../controllers/authentication');
 
-				result.forEach(function(value){
-				  console.log(value);
-				});
-				res.send('plz check the console');
-      } else {
-        res.send('No documents found');
-      }
-      //Close connection
-      db.close();
-			})
-		}
-	})
-})
+// profile
+router.get('/profile', auth, ctrlProfile.profileRead);
+
+// authentication
+router.post('/register', ctrlAuth.register);
+router.post('/login', ctrlAuth.login);
+
+// app.use(function (err, req, res, next) {
+//   if (err.name === 'UnauthorizedError') {
+//     res.status(401);
+//     res.json({"message" : err.name + ": " + err.message});
+//   }
+// });
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// router.get('/thelist',function(req,res){
+// 	var MongoClient= mongodb.MongoClient;
+// 	var url = 'mongodb://localhost:27017/yesy_1'
+// 	MongoClient.connect(url,function(err,db){
+// 		if(err){
+// 			console.log('Unable to connect to the server',err);
+// 		}
+// 		else{
+// 			console.log("Connection Established",url);
+// 			var collection=db.collection('students');
+// 			collection.find({}).toArray(function(err,result){
+// 				if(err){
+// 					res.send(err);
+// 				}
+// 				else if(result.length) {
+
+// 				result.forEach(function(value){
+// 				  console.log(value);
+// 				});
+// 				res.send('plz check the console');
+//       } else {
+//         res.send('No documents found');
+//       }
+//       //Close connection
+//       db.close();
+// 			})
+// 		}
+// 	})
+// })
 
 // router.get('/newstudent',function(req,res){
 // 	res.render('studentList',{student_list_item:studentlist})
