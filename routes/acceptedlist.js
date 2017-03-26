8d2979d5e98b20c8cccc5a6b74ac887ce704c389
Ubuntu <ubuntu@ip-172-31-60-193.ec2.internal>
2017-03-26 16:22:39 +0000

    Updated algorithm for accepting applicant as influencer. Next commit to check ifExceptional

--- routes/acceptedlist.js
+++ routes/acceptedlist.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 console.log('inside accepted list');
+var minNoOfInstagramFollowers = 3000;
+var minNoOfPosts = 50;
+var minAvgLikes = 250;
+var minAvgComments = 20;
 
+var jsonFileName = './dummyacceptedlist.json'
 router.get('/', function(req, res) {
 	var fs = require("fs");
 	// This has to come from APIs in the future.
 	// Hardcoded file being used for now.
-	fs.readFile('./dummyacceptedlist.json', 'utf8', onFileRead);
-	
-	//var jsonfile = require('jsonfile');
-	//var file = './modifiedlist.json';
+	fs.readFile(jsonFileName, 'utf8', onFileRead);
 	
 	var obj = {
 		influencers: []
@@ -21,9 +23,10 @@ router.get('/', function(req, res) {
 			var currentPackage = JSON.parse(data);
 			console.log(currentPackage.categoryName);
 			for(var applicants in currentPackage.applicants) {
-				console.log(applicants + ":" + currentPackage.applicants[applicants].username);
-				if(currentPackage.applicants[applicants].noofinstagramfollowers > 100000 && currentPackage.applicants[applicants].averagenooflikes > 600) {
-					obj.influencers.push(currentPackage.applicants[applicants]);
+				var currApplicant = currentPackage.applicants[applicants];
+				console.log(applicants + ":" + currApplicant.applicantusername);
+				if(minQualification(currApplicant, applicants)) {
+					obj.influencers.push(currApplicant);
 				}
 			}
 			var json = JSON.stringify(obj);
@@ -32,9 +35,31 @@ router.get('/', function(req, res) {
 					return console.error(err);
 				}
 			});
-			//res.render('acceptedlist', { title : 'blah blah' , followercount : currentPackage.categoryName, wholedata : currentPackage } );
 		}
 	}
+	console.log("Let\'s go to the webpage now");
+	res.render('acceptedlist', { title: 'Here\'s your list of Influencers', json: obj });
 });
+
+
+// Function to check if the applicant meets the minimum qualifications required to become an Influencer.
+// data holds the applicant object currently being checked.
+// Following checks done:
+// Does applicant meet:
+// 1. Minimum no of instagram followers?
+// 2. Minimum no of post?
+// 3. Average likes per post?
+// 4. Average comments per post?
+function minQualification(data, index) {
+	if(data.noofinstagramfollowers > minNoOfInstagramFollowers && data.noofposts > minNoOfPosts) {
+		if(data.averagenooflikes > minAvgLikes && data.averagenoofcomments > minAvgComments) {
+			return true;	
+		} else {
+			return false;
+		}		
+	} else {
+		return false;
+	}
+}
 
 module.exports = router;
