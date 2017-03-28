69bb90364659968e462602ddb750939e3f0c17b0
LeenaJawale <leenajawale22@gmail.com>
2017-03-28 18:56:41 -0400

    Added country and state code

---
+++ SourceCode/Frontend/js/profile.js
@@ -0,0 +1,10 @@
+﻿var appProfile = angular.module("appProfile", []);
+appProfile.controller("ctrlProfile", function ($scope, $http) {
+    $http.get("/json/countrystates.json").then(function (response) {          //Read json file
+        $scope.country = response.data.country;
+    });
+
+    $scope.changeCountry = function () {
+        $scope.selectCity = null;
+    }
+});
