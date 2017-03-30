dfe349c00dbcb624cc7abac71bc7fe33e76d556f
LeenaJawale <leenajawale22@gmail.com>
2017-03-29 23:12:52 -0400

    changed app and controller name

--- SourceCode/Frontend/js/statusbox.js
+++ SourceCode/Frontend/js/statusbox.js
@@ -1,12 +1,12 @@
-﻿var appProfile = angular.module("appStatusbox", []);
-appProfile.controller("ctrlStatusbox", function ($scope, $http) {
+var appStatusbox = angular.module("appStatusbox", []);
+appStatusbox.controller("ctrlStatusbox", function ($scope, $http) {
     $http.get("/json/statusbox.json").then(function (response) {          //Read json file
         $scope.statusbox = response.data.statusbox;
     });
 
     $scope.openmodal = function (productID) {
         $scope.productID = productID;
-       alert("hi");
+       // alert(productID);
        $("#Product").modal();
        
     }
