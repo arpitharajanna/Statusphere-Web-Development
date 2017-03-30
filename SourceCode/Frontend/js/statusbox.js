730e36c756c29038831c2cd6cb4dcb8921c0d95e
LeenaJawale <leenajawale22@gmail.com>
2017-03-29 20:25:17 -0400

    added code to read json file

---
+++ SourceCode/Frontend/js/statusbox.js
@@ -0,0 +1,16 @@
+﻿var appProfile = angular.module("appStatusbox", []);
+appProfile.controller("ctrlStatusbox", function ($scope, $http) {
+    $http.get("/json/statusbox.json").then(function (response) {          //Read json file
+        $scope.statusbox = response.data.statusbox;
+    });
+
+    $scope.openmodal = function (productID) {
+        $scope.productID = productID;
+       // alert(productID);
+       $("#Product").modal();
+       
+    }
+    $scope.addtobox = function () {
+        alert("valid");
+    }
+});
