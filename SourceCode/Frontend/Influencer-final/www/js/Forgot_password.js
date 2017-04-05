5fe7bc5be509c09b343c84ae12eb5bdf25e92c72
Sathya Narayanan Amarnath <sathyanarayanan@knights.ucf.edu>
2017-04-05 18:09:29 -0400

    changes-adding js json files
    Changes

---
+++ SourceCode/Frontend/Influencer-final/www/js/Forgot_password.js
@@ -0,0 +1,32 @@
+﻿var app = angular.module("forgotpass_app", []);
+app.controller("forgotpass_ctrl", function ($scope, $http) {
+
+    $scope.forgotpassword = {
+
+
+        clickme: function () {
+            if ($scope.forgotpasword.$invalid)
+                return false;
+            else {
+                /*  var data = $.param({
+                      applicant: JSON.stringify({
+                          username: $scope.name,
+                          email: $scope.email,
+                          password: $scope.password,
+
+                      })
+                  });*/
+                alert($scope.forgotpassword.email);
+                /* $http.post("/", data).success(function (response) {
+                     console.log('Data posted successfully');
+                     alert(response);  
+                     
+                 }); 
+                window.location.href = "http://localhost:57257/Profile.html";*/
+            }
+        }
+
+
+
+    };
+});
---
+++ SourceCode/Frontend/Influencer-final/www/js/todolist.js
@@ -0,0 +1,12 @@
+﻿var appTodolist = angular.module("status_app", []);
+appTodolist.controller("status_ctrl", function ($scope, $http) {
+    $http.get("/json/todolist.json").then(function (response) {          //Read json file
+        $scope.todolist = response.data.todolist;
+    },
+            function (error) {
+                // Handle error here
+                console.log(error.data);
+                alert(error.data.message);
+            }
+    );
+});
