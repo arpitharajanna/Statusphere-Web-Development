55d3988aa21c6b124ea52c5d4e647777d5a11bf4
Sathya Narayanan Amarnath <sathyanarayanan@knights.ucf.edu>
2017-04-04 23:36:52 -0400

    unnnecessary files removed
    files removed

--- SourceCode/Frontend/js/todolist.js
+++
@@ -1,6 +0,0 @@
-﻿var appTodolist = angular.module("appTodolist", []);
-appTodolist.controller("ctrlTodolist", function ($scope, $http) {
-    $http.get("/json/todolist.json").then(function (response) {          //Read json file
-        $scope.todolist = response.data.todolist;
-    });
-});
