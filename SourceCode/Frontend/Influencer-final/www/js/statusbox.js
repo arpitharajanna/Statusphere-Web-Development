7111ed0d9bb453421e7c20b7229720d99e5caf2b
LeenaJawale <leenajawale22@gmail.com>
2017-04-05 00:52:40 -0400

    code for read json file

---
+++ SourceCode/Frontend/Influencer-final/www/js/statusbox.js
@@ -0,0 +1,51 @@
+﻿var appStatusbox = angular.module("appStatusbox", []);
+appStatusbox.controller("ctrlStatusbox", function ($scope, $http, $window) {
+    $http.get("/json/statusbox.json").then(function (response) {          //Read json file
+        $scope.statusbox = response.data.statusbox;
+        
+    });
+   
+    $scope.usern = $window.sessionStorage.getItem("user_name");
+    $scope.openmodal = function (productID) {
+        $scope.productID = productID;
+        // $scope.reqField = 'false';
+        $scope.errormessage = '';
+       $("#Product").modal();
+       
+    }
+    
+    $scope.changestatus = function () {
+        $scope.reqField = 'true';
+        $scope.errormessage = '';
+    }
+    $scope.errormessage = '';
+    $scope.addtobox = function () {
+    
+        if( $scope.reqField == 'true')
+        {
+            //alert("valid");
+            $scope.productid = $scope.productID;
+            alert($scope.productid);
+            var statusboxdata = $.param({
+                statusboxDetails: JSON.stringify({
+                    username: $scope.usern,
+                    productId: $scope.productid
+                })
+            });
+            alert(statusboxdata);
+
+            /* $http.post("/Startup", data).success(function (data, status) {
+                 console.log('Data posted successfully');
+             })*/
+
+        }
+        else
+        {
+            alert($scope.reqField);
+           // alert("invalid");
+            $scope.errormessage = "Please select Product agreement";
+        }
+        
+       
+    }
+});
