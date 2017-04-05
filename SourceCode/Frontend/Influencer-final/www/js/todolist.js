bdd32473322c1ac5f55c4e6faddb2d996ef9fbbc
LeenaJawale <leenajawale22@gmail.com>
2017-04-05 01:02:30 -0400

    code to read json file

---
+++ SourceCode/Frontend/Influencer-final/www/js/todolist.js
@@ -0,0 +1,6 @@
+﻿var appTodolist = angular.module("appTodolist", []);
+appTodolist.controller("ctrlTodolist", function ($scope, $http) {
+    $http.get("/json/todolist.json").then(function (response) {          //Read json file
+        $scope.todolist = response.data.todolist;
+    });
+});
