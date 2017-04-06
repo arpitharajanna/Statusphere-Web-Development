b22dd866765c7005b59eb53eb8e229134e1528da
LeenaJawale <leenajawale22@gmail.com>
2017-04-05 23:57:08 -0400

    updated profilediplay json read

--- SourceCode/Frontend/Influencer-final/www/js/todolist.js
+++ SourceCode/Frontend/Influencer-final/www/js/todolist.js
@@ -1,5 +1,6 @@
 var appTodolist = angular.module("status_app", []);
 appTodolist.controller("status_ctrl", function ($scope, $http) {
+
     $http.get("/json/todolist.json").then(function (response) {          //Read json file
         $scope.todolist = response.data.todolist;
     },
@@ -9,4 +10,18 @@ appTodolist.controller("status_ctrl", function ($scope, $http) {
                 alert(error.data.message);
             }
     );
+   // alert("hi");
+    $http.get('/json/Profile.json').then(function (response) {
+        $scope.userinfo = response.data.userinfo;
+    });
+
+    $http.get("/json/statusbox.json").then(function (response) {          //Read json file
+        $scope.statusbox = response.data.statusbox;
+
+    },
+                                                function (error) {
+                                                    // Handle error here
+                                                    console.log(error.data);
+                                                    alert(error.data.message);
+                                                });
 });
