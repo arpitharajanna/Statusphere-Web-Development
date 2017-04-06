3071c6a3ab926e132deece809fe7b71cd637822e
LeenaJawale <leenajawale22@gmail.com>
2017-04-06 15:26:52 -0400

    updated code for modal

--- SourceCode/Frontend/Influencer-final/www/js/todolist.js
+++ SourceCode/Frontend/Influencer-final/www/js/todolist.js
@@ -1,7 +1,7 @@
 var appTodolist = angular.module("status_app", []);
-
 appTodolist.controller("status_ctrl", function ($scope, $http,$window) {
-    $http.get("/Json/todolist.json").then(function (response) {          //Read json file - todolist.json
+
+    $http.get("/json/todolist.json").then(function (response) {          //Read json file
         $scope.todolist = response.data.todolist;
     },
             function (error) {
@@ -10,23 +10,62 @@ appTodolist.controller("status_ctrl", function ($scope, $http,$window) {
                 alert(error.data.message);
             }
     );
-
-
-    $http.get('/Json/Profile.json').then(function (response) {
-
+   // alert("hi");
+    $http.get('/json/Profile.json').then(function (response) {
         $scope.userinfo = response.data.userinfo;
-
-
     });
 
-
-    $scope.usern = $window.sessionStorage.getItem("user_name");
-    $http.get("/Json/statusbox.json").then(function (response) {          //Read json file
+    $http.get("/json/statusbox.json").then(function (response) {          //Read json file
         $scope.statusbox = response.data.statusbox;
-      
-    });
+
+    },
+                                                 function (error) {
+                                                     // Handle error here
+                                                     console.log(error.data);
+                                                     alert(error.data.message);
+                                                 });
+
+    //  $scope.usern = $window.sessionStorage.getItem("user_name");
+    $scope.usern = $window.localStorage.getItem("user_name");
+    $scope.openmodal = function (productID) {
+        $scope.productID = productID;        
+        $scope.errormessage = '';
+      //  alert(productID);
+        $("#Product").modal();
+    }
+
+    $scope.changestatus = function () {
+        //$scope.reqField = 'true';
+        $scope.errormessage = '';
+    }
+    $scope.errormessage = '';
+    $scope.addtobox = function () {
+        alert($scope.checked);
+
+        if ($scope.checked) {
+            var statusboxdata = {
+                username: $scope.username,
+                product_Id: $scope.productID
+            }
+            //alert(statusboxdata);
+
+            $http.post("", statusboxdata).then(function (res) {
+                console.log('Data posted successfully');
+                alert("Package has been added")
+            },
+               function (error) {
+                   // Handle error here
+                   console.log(error.data);
+                   alert(error.data.message);
+               });
+
+        }
+        else {
+            alert($scope.reqField);
+            // alert("invalid");
+            $scope.errormessage = "Please select Product agreement";
+        }
 
 
-
-
+    }
 });
