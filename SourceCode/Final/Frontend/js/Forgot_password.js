daf041ce26442a6af976b42967fa565071f89f2b
LeenaJawale <leenajawale22@gmail.com>
2017-04-10 12:38:25 -0400

    update for integration

--- SourceCode/Final/Frontend/js/Forgot_password.js
+++ SourceCode/Final/Frontend/js/Forgot_password.js
@@ -1,37 +1,58 @@
 var app = angular.module("forgotpass_app", []);
-app.controller("forgotpass_ctrl", function ($scope, $http,$window) {
+app.controller("forgotpass_ctrl", function ($scope, $http, $window) {
+    /*  $scope.forgotpassword = {
+          click: function () {
+              if ($scope.forgotpassword.$invalid)
+                  return false;
+              else {
+                  /*  var data = $.param({
+                        applicant: JSON.stringify({
+                            username: $scope.name,
+                            email: $scope.email,
+                            password: $scope.password,
+                        })
+                    }); */
+    //    alert("sucess");
+    /* $http.post("/", data).success(function (response) {
+         console.log('Data posted successfully');
+         alert(response);  
+         
+     }); */
+    /*  window.location.href = "http://localhost:57257/Code.html";
+  }
+}
+};*/
+    /* $scope.forgotpassword = {
+          clickme: function () {
+              if ($scope.forgotpasword.$invalid)
+                  return false;
+              else {*/
+    /*  var data = $.param({
+          applicant: JSON.stringify({
+              username: $scope.name,
+              email: $scope.email,
+              password: $scope.password,
+          })
+      });*/
+    //  alert($scope.forgotpassword.email);
+    /* $http.post("/", data).success(function (response) {
+         console.log('Data posted successfully');
+         alert(response);  
+         
+     });*/
 
-  /*  $scope.forgotpassword = {
-        clickme: function () {
-            if ($scope.forgotpasword.$invalid)
-                return false;
-            else {
-                /*  var data = $.param({
-                      applicant: JSON.stringify({
-                          username: $scope.name,
-                          email: $scope.email,
-                          password: $scope.password,
-                      })
-                  });*/
-              //  alert($scope.forgotpassword.email);
-                /* $http.post("/", data).success(function (response) {
-                     console.log('Data posted successfully');
-                     alert(response);  
-                     
-                 }); 
-                window.location.href = "http://localhost:57257/Profile.html";*/
-        /*    }
-        }
-
-
-
-    };*/
+    /*  }
+    }
+};*/
 
     $scope.forgotpassword = function () {
-                          
-          //  $("#message").modal();
 
-        confirm("You will receive notification to your registered email");
-        window.location.href = "PaswordReset.html";
+        //  $("#message").modal();
+        if ($scope.email.$invalid)
+            return false;
+        else {
+            confirm("You will receive notification to your registered email");
+            window.location.href = "Code.html";
+        }
     }
 });
