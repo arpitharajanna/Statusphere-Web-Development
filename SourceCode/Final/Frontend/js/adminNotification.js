5402287985259636cbfa192e423e48c3dd449d92
Atul John <Atul John>
2017-04-11 15:55:18 -0400

    updated files which were in SourceCode/Final

--- SourceCode/Final/Frontend/js/adminNotification.js
+++ SourceCode/Final/Frontend/js/adminNotification.js
@@ -1,19 +1,29 @@
-// JSON Object for sent notification messages  , This JSON object to be replaced with the one we receive from Mongodb
-var notificationMsgObj = { "messages": [ 
-                {"email": "email1@test.com",
-                "message":"Just Text Messages1"},
-                {"email": "email2@test.com",
-                "message":"Just Text Messages2"},
-                {"email": "email3@test.com",
-                "message":"Just Text Messages3"},
-                {"email": "email4@test.com",
-                "message":"Just Text Messages4"},
-                {"email": "email5@test.com",
-                "message":"Just Text Messages5"}
-                ]
-              }
-function getSentMessage()
-{
+﻿// JSON Object for sent notification messages  , This JSON object to be replaced with the one we receive from Mongodb
+var notificationMsgObj = {
+    "messages": [
+                    {
+                        "email": "email1@test.com",
+                        "message": "Just Text Messages1"
+                    },
+                    {
+                        "email": "email2@test.com",
+                        "message": "Just Text Messages2"
+                    },
+                    {
+                        "email": "email3@test.com",
+                        "message": "Just Text Messages3"
+                    },
+                    {
+                        "email": "email4@test.com",
+                        "message": "Just Text Messages4"
+                    },
+                    {
+                        "email": "email5@test.com",
+                        "message": "Just Text Messages5"
+                    }
+    ]
+}
+function getSentMessage() {
     // Remove Existing Content from DIV
     $("#divSentMagTable").html("");
 
@@ -25,9 +35,8 @@ function getSentMessage()
                 </thead>\
                 <tbody>\
                 ";
-    for ( mIndex in notificationMsgObj.messages)
-    {
-      //alert(notificationMsgObj.messages[mIndex].email);
+    for (mIndex in notificationMsgObj.messages) {
+        //alert(notificationMsgObj.messages[mIndex].email);
         table += "<tr><td><input type='text' value='" + notificationMsgObj.messages[mIndex].email + "' readonly </td>";
         table += "<td><textarea  readonly rows='2' cols='50' >" + notificationMsgObj.messages[mIndex].message + "</textarea></td></tr>"
 
