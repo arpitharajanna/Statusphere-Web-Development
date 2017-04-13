bd1e8f0744fa0fa097b5930ef4ec12482cfaa37f
pavitra39 <pavitra_3994@Knights.ucf.edu>
2017-04-13 14:14:48 -0400

    Add files via upload

---
+++ SourceCode/Final/Frontend/js/applicant_info.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module('myapp', []);
+app.controller('myCtrl', function ($scope, $http) {
+    $http.get("json/applicant_info.json")
+    .then(function (response) {
+        $scope.applicant_info = response.data.applicant_info;
+    });
+    //alert("hi");
+});
