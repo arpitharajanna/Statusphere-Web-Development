870e5e850d2693343634b20e98faead6d1907e8a
pavitra39 <pavitra_3994@Knights.ucf.edu>
2017-04-13 19:46:31 -0400

    Update influencer-package.js

--- SourceCode/Final/Frontend/js/influencer-package.js
+++ SourceCode/Final/Frontend/js/influencer-package.js
@@ -1,8 +1,39 @@
-﻿var app = angular.module('myapp', []);
-app.controller('myCtrl', function ($scope, $http) {
-    $http.get("/json/influencer-package.json")
-    .then(function (response) {
-        $scope.influencer_package = response.data.influencer_package;
-    });
-    alert("hi");
+var app = angular.module('myapp', []);
+app.controller('myCtrl', function ($scope) {
+
+    $scope.obj = [
+
+          {
+              "Firstname": "hello",
+              "Lastname": "howare",
+              "Packagename": "uoi",
+              "Duedate": "04-05-2017",
+              "Accepteddate": "04-23-2017",
+              "Status": "accepted"
+
+          },
+          {
+              "Firstname": "llo",
+              "Lastname": "are",
+              "Packagename": "uoi",
+              "Duedate": "04/05/2017",
+              "Accepteddate": "04/23/2017",
+              "Status": "accepted"
+
+          },
+           {
+               "Firstname": "abc",
+               "Lastname": "re",
+               "Packagename": "uoi",
+               "Duedate": "04-05-2017",
+               "Accepteddate": "04-23-2017",
+               "Status": "accepted"
+
+           }
+    ];
+    
+
+        
+    
+    //alert("hi");
 });
