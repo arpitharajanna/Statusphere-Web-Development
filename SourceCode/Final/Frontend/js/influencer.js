7654cd18e81964e0867c7406eae3fad581f3e237
pavitra39 <pavitra_3994@Knights.ucf.edu>
2017-04-13 19:48:17 -0400

    Update influencer.js

--- SourceCode/Final/Frontend/js/influencer.js
+++ SourceCode/Final/Frontend/js/influencer.js
@@ -1,7 +1,46 @@
-﻿var myapp = angular.module("myapp", []);
-myapp.controller("myCtrl", function ($scope, $http) {
-    $http.get("/json/influencer.json").then(function (response) {          //Read json file
-        $scope.names = response.data.names;
-    });
+    var app = angular.module('myapp', []);
+    app.controller('myCtrl', function ($scope) {
+
+        $scope.names =  [
+
+      {   
+
+   "Firstname": "meoutside",
+       "Lastname": "poaaeside",
+       "InstagramID": "wqesidegirl",
+       "EmailID" : "abbvsh@gmail.com",
+       "NoInstagramFollowers": 320000,
+       "Age": 23,
+       "Gender": "male",
+       "DateAccepted": "06-01-2017"
+       },
+
+   {
+
+       "Firstname": "outside",
+       "Lastname": "aeside",
+       "InstagramID": "sidegirl",
+       "EmailID": "qwrtwsh@gmail.com",
+       "NoInstagramFollowers": 320,
+       "Age": 23,
+       "Gender": "male",
+       "DateAccepted": "06-01-2017"
+
+   },
+
+   {
+       
+       "Firstname": "side",
+       "Lastname": "aide",
+       "InstagramID": "degirl",
+       "EmailID": "pquwsh@gmail.com",
+       "NoInstagramFollowers": 3200,
+       "Age": 23,
+       "Gender": "male",
+       "DateAccepted": "06-01-2017"
+
+   }
+   
+    ]
     //alert("hi");
 });
