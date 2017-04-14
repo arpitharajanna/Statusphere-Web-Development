787c8d23fe2474be66ffd03638bae601806569e4
poojaShivashankar <poojshankar@gmail.com>
2017-04-13 19:56:31 -0500

    Add files via upload

---
+++ SourceCode/Final/Frontend/js/Package.js
@@ -0,0 +1,49 @@
+﻿var myapp = angular.module('myapp', []);
+myapp.controller('myCtrl', function ($scope) {
+
+    $scope.names =
+    [
+
+      {
+          "Name": "hello",
+          "QuantityAccepted": "2",
+          "QuantityAvailable": "3",
+          "DateCreated": "04-23-2017",
+          "DateDue": "04-23-2017",
+          "PackageStatus": "accepted"
+
+      },
+
+      {
+          "Name": "pollo",
+          "QuantityAccepted": "2",
+          "QuantityAvailable": "3",
+          "DateCreated": "04-23-2017",
+          "DateDue": "08-23-2017",
+          "PackageStatus": "accepted"
+
+      },
+
+      {
+          "Name": "awlo",
+          "QuantityAccepted": "2",
+          "QuantityAvailable": "3",
+          "DateCreated": "04-23-2017",
+          "DateDue": "02-23-2017",
+          "PackageStatus": "accepted"
+
+      },
+      {
+          "Name": "llo",
+          "QuantityAccepted": "2",
+          "QuantityAvailable": "3",
+          "DateCreated": "04-23-2017",
+          "DateDue": "04-23-2017",
+          "PackageStatus": "accepted"
+
+
+      }
+
+    ]
+    ;
+});
