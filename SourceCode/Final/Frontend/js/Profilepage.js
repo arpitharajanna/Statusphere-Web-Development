a3969fb974dad6d7bcd36e9c387a39ba0581b464
Sathya-amarnath <sathyanarayanan@knights.ucf.edu>
2017-04-15 01:51:04 -0400

    updated image validation containing js

--- SourceCode/Final/Frontend/js/Profilepage.js
+++ SourceCode/Final/Frontend/js/Profilepage.js
@@ -1,66 +1,29 @@
-var app = angular.module("appProfile", []);
-app.controller('ctrlProfile', function ($scope, $http, $window) {
-    $http.get('/Json/Countries.json').then(function (response) {
-
-        $scope.countries = response.data.countries;
-    });
-    $http.get('/Json/States.json').then(function (response) {
-
-        $scope.states = response.data.states;
-    });
-    $scope.GetSelectedCountry = function () {
-        $scope.strCountry = $scope.form.selectCountry;
-    };
-
-
-    $scope.someSelected = function (object) {
-        if (!object) return false;
-        return Object.keys(object).some(function (key) {
-            return object[key];
-        });
-    }
-
-    $scope.doTouched = function () {
-        $scope.theForm.subscribe.$setTouched();
-    }
-
-    $scope.updateProfile = function () {
-        alert("success");
-    }
-
-
-});
-
-app.directive('myUpload', [function () {
-    return {
-        restrict: 'A',
-        link: function (scope, elem, attrs) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                scope.image = e.target.result;
-                scope.$apply();
-            }
-
-            elem.on('change', function () {
-                reader.readAsDataURL(elem[0].files[0]);
-            });
-        }
-    };
-}]);
-
 app.directive('validFile', function () {
     return {
         require: 'ngModel',
-        link: function (scope, el, attrs, ctrl) {
-            ctrl.$setValidity('validFile', el.val() != '');
-            //change event is fired when file is selected
-            el.bind('change', function () {
-                ctrl.$setValidity('validFile', el.val() != '');
+        link: function (scope, elem, attrs, ngModel) {
+            var validFormats = ['jpg', 'jpeg', 'png'];
+            elem.bind('change', function () {
+                validImage(false);
                 scope.$apply(function () {
-                    ctrl.$setViewValue(el.val());
-                    ctrl.$render();
+                    ngModel.$render();
                 });
             });
+            ngModel.$render = function () {
+                ngModel.$setViewValue(elem.val());
+            };
+            function validImage(bool) {
+                ngModel.$setValidity('extension', bool);
+            }
+            ngModel.$parsers.push(function (value) {
+                var ext = value.substr(value.lastIndexOf('.') + 1);
+                if (ext == '') return;
+                if (validFormats.indexOf(ext) == -1) {
+                    return value;
+                }
+                validImage(true);
+                return value;
+            });
         }
-    }
+    };
 });
