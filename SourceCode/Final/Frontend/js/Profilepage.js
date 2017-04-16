9b69d86989754c3a73311854e5765e68906f0712
gkataria19 <gaurav.kataria@knights.ucf.edu>
2017-04-16 11:30:08 -0400

    Update Profilepage.js

--- SourceCode/Final/Frontend/js/Profilepage.js
+++ SourceCode/Final/Frontend/js/Profilepage.js
@@ -51,8 +51,8 @@ app.controller('ctrlProfile', function ($scope, $http, $window) {
             Blog: $scope.form.blog,
             AddressLine1: $scope.form.addr1,
             AddressLine2: $scope.form.addr2,
-            Country: $scope.form.selectCountry,
-            State: $scope.form.selectstate,
+            Country: $scope.form.selectCountry.name,
+            State: $scope.form.selectstate.name,
             City: $scope.form.city,
             ZIPCode: $scope.form.zip,
             mob: $scope.form.phno,
@@ -63,7 +63,7 @@ app.controller('ctrlProfile', function ($scope, $http, $window) {
         }
         $http.post("/editProfile", data).then(function (response) {
             console.log('Data posted successfully');
-            alert("success");},
+            alert(response.data.message);},
                 function (error) {
 
                     //$scope.errorm = error.data.message ;
