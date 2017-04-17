8a425a74e39938a3b77152e57ee58dfde67b7f80
somnath <saha.somnath@gmail.com>
2017-04-16 23:24:23 -0400

    Updated package to read data from DB using API

--- SourceCode/Final/Frontend/js/Package.js
+++ SourceCode/Final/Frontend/js/Package.js
@@ -50,7 +50,14 @@ var packageListObj = [
     ]
 
 var myapp = angular.module('myapp', []);
-myapp.controller('myCtrl', function ($scope) {
+myapp.controller('myCtrl', function ($scope,$http) {
 
-    $scope.names = packageListObj;
+    //$scope.names = packageListObj;  //Old one 
+
+    // Generally, we expect all the above fields for Packagelist, which , presently not being transferred.
+    $http.get('http://localhost:3000/api/packagelist').then(function(response) {
+            //$scope.names = response.data; // To be UPdated when there is any data into the database, and all the required fields matches.
+            $scope.names = packageListObj;
+        });
+    
 });
