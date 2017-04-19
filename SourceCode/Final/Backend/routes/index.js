cf26b2ae3b8bdeb02e37f9b1c93024e47c46cdc6
gkataria19 <gaurav.kataria@knights.ucf.edu>
2017-04-19 15:43:02 +0000

    Getting all products

--- SourceCode/Final/Backend/routes/index.js
+++ SourceCode/Final/Backend/routes/index.js
@@ -21,6 +21,7 @@ var edit_Product=require('../controllers/Edit_Product');
 var ctrlNotification = require('../controllers/notification');
 var ctrlPackages     = require('../routes/api/packagelist'); // New controller script required for package
 var Influencer_Profile= require('../controllers/Influencer_profile');
+var getAllProducts=require('../controllers/getAllProducts');//controller for getting products
 
 
 
@@ -34,9 +35,10 @@ router.post('/login', ctrlAuth.login);
 router.post('/product',create_Product.Create_Product);
 router.post('/package',add_Package.AddPackage);
 router.post('/editProduct',edit_Product.Edit_Product);
-router.get('/todolist',todolist.getProductsofuserinterest);
+router.post('/todolist',todolist.getProductsofuserinterest);
 router.post('/influencer_profile',Influencer_Profile.influencer_profile);
 router.post('/editinfluencer_profile',Influencer_Profile.profileEdit);
+router.get('/getAllProducts',getAllProducts.getAllProducts);//
 
 
 // we can use this for sending notification message and storing data into databse. 
