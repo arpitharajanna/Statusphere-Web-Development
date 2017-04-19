887146e24795bbf5b50a0daecd8c9c9b4dced965
somnath <saha.somnath@gmail.com>
2017-04-19 14:29:57 -0400

    Cleaned up comments and alert message

--- SourceCode/Final/Frontend/js/adminNotification.js
+++ SourceCode/Final/Frontend/js/adminNotification.js
@@ -1,30 +1,4 @@
 ﻿// JSON Object for sent notification messages  , This JSON object to be replaced with the one we receive from Mongodb
-var notificationMsgObj = {
-    "messages": [
-                    {
-                        "email": "email1@test.com",
-                        "message": "Just Text Messages1"
-                    },
-                    {
-                        "email": "email2@test.com",
-                        "message": "Just Text Messages2"
-                    },
-                    {
-                        "email": "email3@test.com",
-                        "message": "Just Text Messages3"
-                    },
-                    {
-                        "email": "email4@test.com",
-                        "message": "Just Text Messages4"
-                    },
-                    {
-                        "email": "email5@test.com",
-                        "message": "Just Text Messages5"
-                    }
-    ]
-}
-
-
 
 var app = angular.module('myapp', []);
 app.controller('myCtrl', function ($scope,$http) {
