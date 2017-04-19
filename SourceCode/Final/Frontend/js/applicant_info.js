cb0453ed4073c3a9125e522ed04e1cfa244cc797
somnath <saha.somnath@gmail.com>
2017-04-19 11:31:33 -0400

    orking copy of Applicant

--- SourceCode/Final/Frontend/js/applicant_info.js
+++ SourceCode/Final/Frontend/js/applicant_info.js
@@ -1,8 +1,16 @@
 ﻿var app = angular.module('myapp', []);
 app.controller('myCtrl', function ($scope, $http) {
-    $http.get("json/applicant_info.json")
-    .then(function (response) {
-        $scope.applicant_info = response.data.applicant_info;
-    });
-    //alert("hi");
+	alert("hiby");
+	$scope.getApplicantInfo = function(applicantName)
+	{
+	alert("hi");
+
+			var applicantName = 'atuljohn';
+	    	$http.get("http://localhost:3000/api/applicantlist/atuljohn").then(function (response) {
+	        	$scope.applicant_info = response.data;
+
+			});
+	    
+	}
+
 });
--- SourceCode/Final/Frontend/js/applicant_list.js
+++ SourceCode/Final/Frontend/js/applicant_list.js
@@ -1,54 +1,40 @@
 var app = angular.module('myapp', []);
-app.controller('myCtrl', function ($scope) {
+app.controller('myCtrl', function ($scope,$http) {
 
-    $scope.applicants =  [
-
-  {
-      "Firstname": "caeside",
-      "Lastname": "caeside",
-      "InstagramID": "mutsidegirl",
-      "NoInstagramFollowers": 32890,
-      "Age": 23,
-      "Gender": "Female",
-      "DateApplied": "06-01-2017"
-
-  },
-  {
-      "Firstname": "caesqidqiide",
-      "Lastname": "dhdihqide",
-      "InstagramID": "cashl",
-      "NoInstagramFollowers": 3250,
-      "Age": 23,
-      "Gender": "Female",
-      "DateApplied": "04-01-2017"
-
-  },
-
-  {
-      "Firstname": "qwetrde",
-      "Lastname": "hiuefhieweside",
-      "InstagramID": "abcde",
-      "NoInstagramFollowers": 345250,
-      "Age": 23,
-      "Gender": "Female",
-      "DateApplied": "04-01-2017"
-
-  },
-
-  {
-      "Firstname": "ceufuefwside",
-      "Lastname": "poiucaeside",
-      "InstagramID": "cairl",
-      "NoInstagramFollowers": 398250,
-      "Age": 23,
-      "Gender": "Female",
-      "DateApplied": "04-01-2017"
-
-  }
-
-    ]
+	// Get all applicant information
+    $http.get('api/applicantlist').then(function(response) {
+            $scope.applicants = response.data; // To be UPdated when there is any data into the database, and all the required fields matches.
+        });
 
 
 
-    //alert("hi");
+    alert(localStorage.getItem("username"));
+    //$scope.applicant_info = $scope.applicants[1];
+    $scope.getApplicantInfo = function(applicant)
+    {
+    	//$scope.applicant_info = applicant;
+    	alert("sadasjhd" + applicant);
+    	//$("#divViewApplicantProfile").modal();
+
+    }
+
+    // Applicant info
+    
+    /*$scope.getApplicantInfo = function(applicantName)
+	{
+	alert("hi" + applicantName);
+
+			var applicantPath = "http://localhost:3000/api/applicantlist/" + applicantName;
+			$http.get(applicantPath).then(function (response) {
+	        	$scope.applicant_info = response.data;
+	        	alert("data:" + applicantPath + " : " + response.data.username);
+	        	$("#divViewApplicantProfile").modal();
+
+			});
+	    	$http.get("http://localhost:3000/api/applicantlist/atuljohn").then(function (response) {
+	        	$scope.applicant_info = response.data;
+
+			});
+	    
+	}*/
 });
